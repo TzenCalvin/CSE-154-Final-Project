58d659db53f6db126fc6d91e8625292719244844
hcking <hcking@uw.edu>
2023-05-31 00:24:22 -0700

    layout toggle with bugs :(

--- app.js
+++ app.js
@@ -19,7 +19,34 @@ app.use(multer().none());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-//add get all endpoint
+const SERVER_SIDE_ERROR_MSG = 'Oh no! An error occurred on the server. Try again later.';
+const SERVER_SIDE_ERROR_STATUS_CODE = 500;
+
+/**
+ * Establishes a database connection to the database and returns the database object.
+ * Any errors that occur should be caught in the function that calls this one.
+ * @returns {sqlite3.Database} - The database object for the connection.
+ */
+async function getDBConnection() {
+  const db = await sqlite.open({
+    filename: 'fp.db',
+    driver: sqlite3.Database
+  });
+
+  return db;
+}
+
+app.get('/all/products', async (req, res) => {
+  try {
+    let db = await getDBConnection();
+    let qry = 'SELECT name, shortname, price FROM products';
+    let result = await db.all(qry);
+    res.type('json').send(result);
+  } catch (err) {
+    res.status(SERVER_SIDE_ERROR_STATUS_CODE);
+    res.type('text').send(SERVER_SIDE_ERROR_MSG);
+  }
+});
 
 app.use(express.static('public'));
 const PORT = process.env.PORT || 8000;
--- public/index.js
+++ public/index.js
@@ -18,6 +18,9 @@
       productButton[i].addEventListener("click", switchProductView);
     }
     productBackButton.addEventListener("click", switchProductView);
+    id('view-all').addEventListener('click', requestAllProducts);
+    id('main-view-back-button').addEventListener('click', goHome);
+    id('layout-button').addEventListener('click', toggleLayout);
 
     /**
      * make function for search
@@ -27,6 +30,86 @@
      * make funtion for adding to cart
      * id("add-to-cart-button").addEventListener("click", doSomething);
      */
+  }
+
+  function toggleLayout() {
+    if (id('main-view-products').classList.contains('product-grid')) {
+      id('main-view-products').classList.remove('product-grid');
+      id('main-view-products').classList.add('product-list');
+      let products = qsa('#main-view-products article');
+      for (let i = 0; i < products.length; i++) {
+        products[i].classList.remove('product-card');
+        products[i].classList.add('product-list-item');
+      }
+      let productsInfo = qsa('#main-view-products article section');
+      for (let i = 0; i < products.length; i++) {
+        products[i].classList.remove('product-info-grid');
+        products[i].classList.add('product-info-list');
+      }
+    } else {
+      id('main-view-products').classList.remove('product-list');
+      id('main-view-products').classList.add('product-grid');
+      let products = qsa('#main-view-products article');
+      for (let i = 0; i < products.length; i++) {
+        products[i].classList.remove('product-list-item');
+        products[i].classList.add('product-card');
+      }
+      let productsInfo = qsa('#main-view-products article section');
+      for (let i = 0; i < products.length; i++) {
+        products[i].classList.remove('product-info-list');
+        products[i].classList.add('product-info-grid');
+      }
+    }
+  }
+
+  /**
+   * Takes the user back to the home page from the main view of all products page.
+   */
+  function goHome() {
+    id('main-view').classList.add('hidden');
+    id('menu-page').classList.remove('hidden');
+    id('main-view-products').innerHTML = '';
+  }
+
+  /**
+   * Gets all of the products names, shortnames, and prices from the API.
+   */
+  async function requestAllProducts() {
+    try {
+      let allProducts = await fetch('/all/products');
+      await statusCheck(allProducts);
+      allProducts = await allProducts.json();
+      displayAllProducts(allProducts);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  /**
+   * Display all of the products from the API.
+   * @param {JSON} allProducts - all of the products names, shortnames, and prices
+   */
+  function displayAllProducts(allProducts) {
+    id('menu-page').classList.add('hidden');
+    id('main-view').classList.remove('hidden');
+    for (let i = 0; i < allProducts.length; i++) {
+      let product = gen('article');
+      product.classList.add('product-card');
+      let image = gen('img');
+      image.src = 'images/' + allProducts[i].shortname + '.png';
+      image.alt = allProducts[i].name;
+      product.appendChild(image);
+      let info = gen('section');
+      info.classList.add('product-info-grid');
+      let name = gen('p');
+      name.textContent = allProducts[i].name;
+      info.appendChild(name);
+      let price = gen('p');
+      price.textContent = '$' + allProducts[i].price;
+      info.appendChild(price);
+      product.appendChild(info);
+      id('main-view-products').appendChild(product);
+    }
   }
 
   /**
@@ -43,6 +126,18 @@
   function switchProductView() {
     id("menu-page").classList.toggle("hidden");
     id("product-page").classList.toggle("hidden");
+  }
+
+  /**
+   * Checks to make sure that the response from the API is OK
+   * @param {Promise<object>} res - response from the API
+   * @returns {Promise<object>} - OK response from the API
+   */
+  async function statusCheck(res) {
+    if (!res.ok) {
+      throw new Error(await res.text());
+    }
+    return res;
   }
 
   // HELPER FUNCTIONS
@@ -72,5 +167,14 @@
    */
   function qsa(selector) {
     return document.querySelectorAll(selector);
+  }
+
+  /**
+   * Returns a new element with the given tag name.
+   * @param {string} tagName - HTML tag name for new DOM element.
+   * @returns {object} New DOM object for given HTML tag.
+   */
+  function gen(tagName) {
+    return document.createElement(tagName);
   }
 })();
